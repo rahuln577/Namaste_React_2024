f93da4ba6d6236c3663ab11bfd49cb211990050a
rahuln577 <rahulrahuln2001@gmail.com>
2024-07-02 19:53:00 +0530

    upated spelling mistake

--- Episode-1/app.js
+++ Episode-1/app.js
@@ -1,8 +1,8 @@
-const heading1 = React.createElement("h1", {}, "hello world im h1");
+const heading1 = React.createElement("h1", {}, "hello world  in h1");
 const child1 = React.createElement("div", { id: "child1" }, heading1);
 const parent1 = React.createElement("div", { id: "parent1" }, child1);
 
-const heading2 = React.createElement("h2", {}, "hello i, h2");
+const heading2 = React.createElement("h2", {}, "hello in h2");
 const child2 = React.createElement("div", { id: "child2" }, heading2)
 const parent2 = React.createElement("div", { id: "parent2" }, child2);
 
@@ -10,4 +10,4 @@ const grandpa = React.createElement("div", { id: "granddad" }, [parent1, parent2
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(grandpa);
+root.render(grandpa);			
