c9e5bb6d98ff63fbbc4bcf1d68f7408cb9194d51
shyamg090 <samshyam9630@gmail.com>
2024-07-02 17:40:29 +0530

    Day-1 and Episode-1 completed

---
+++ Episode-1/app.js
@@ -0,0 +1,13 @@
+const heading1 = React.createElement("h1", {}, "hello world im h1");
+const child1 = React.createElement("div", { id: "child1" }, heading1);
+const parent1 = React.createElement("div", { id: "parent1" }, child1);
+
+const heading2 = React.createElement("h2", {}, "hello i, h2");
+const child2 = React.createElement("div", { id: "child2" }, heading2)
+const parent2 = React.createElement("div", { id: "parent2" }, child2);
+
+const grandpa = React.createElement("div", { id: "granddad" }, [parent1, parent2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(grandpa);
